803eefdd253a648ab876398e7ff20fe5092c9524
Dane <dan.maldonado.2132@lacetania.cat>
2023-12-11 19:02:19 +0100

    Cambios

--- SamuelDan_MP03UF2_Matrius.py
+++ SamuelDan_MP03UF2_Matrius.py
@@ -1,46 +1,46 @@
-# Fes un programa en Python amb un menú per fer la crida a cadascuna de les següents funcions. 
-# Cap de les funcions té return. El programa serà amb estructura main.
-# Dan Maldonado - 3/12/2023
-
 import random
+import utils
 
-# Funció de multiplicació
-def crear_matriu_2d(n1,n2):
-    '''Imprimeix el resultat de multiplicar dos números passats 
-    com a paràmetres, els inputs es fan a fora de la funció.'''
-    m = [[random.randint(1,100) for i in range(n2)] for i in range(n1)]
+# Ex1
+def crear_matriu_2d(n1, n2):
+    m = [[random.randint(1, 100) for i in range(n2)] for i in range(n1)]
     print(m)
+    return m
 
+# Ex2
+def print_matrix(m):
+    for row in m:
+        print(*row)
 
+# Ex3
+def llistar_multiples(m,n):
+    multiples = [num for row in m for num in row if num % n == 0]
+    return multiples
+
+# Ex4
 def fer_llista_unica(m):
     m_unica = list(set([num for row in m for num in row]))
     return m_unica
 
-
-def cercar_els_que_acaben_en(m,e):
-    None
+# Ex5
+def buscar_min_i_max(m):
+    m1d = [num for row in m for num in row]
+    return(min(m1d),max(m1d))
 
 # Funció principal
 def main():
-    '''Anem demanant que vol fer l'usuari i
-    depenent la resposta inicialitzem una funció
-    o un altre.'''
-    
     n1 = int(input("Introdueix el primer nombre: "))
     n2 = int(input("Introdueix el segon nombre: "))
-    m = crear_matriu_2d(n1,n2)
+    m = crear_matriu_2d(n1, n2)
     
-
+    print(print_matrix(m))
+    
+    print(llistar_multiples(m,n))
+    
     print(fer_llista_unica(m))
-
-
     
-    print(cercar_els_que_acaben_en(m))
+    print(buscar_min_i_max(m))
     
-
-    
-    print(cercar_els_que_acaben_en(m))    
-        
     
 if __name__ == "__main__":
     main()
