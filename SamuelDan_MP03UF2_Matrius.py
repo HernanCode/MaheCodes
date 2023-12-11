692c2df43a04d1c11d9b989987d267cec06eddb6
Dane <dan.maldonado.2132@lacetania.cat>
2023-12-11 18:48:23 +0100

    he hecho cambios

---
+++ SamuelDan_MP03UF2_Matrius.py
@@ -0,0 +1,58 @@
+# Fes un programa en Python amb un menú per fer la crida a cadascuna de les següents funcions. 
+# Cap de les funcions té return. El programa serà amb estructura main.
+# Dan Maldonado - 3/12/2023
+
+import random
+
+menu = ("Matriu 2D", "Anys 2000", "Gira del revés", "Llista unica",)
+
+# Creem una funció per mostrar el menú
+def print_menu(menu):
+    '''Mostra les entrades de la tupla "menu" afegint el nombre
+    corresponent i la entrada "SORTIR" al final.'''
+    mida = len(menu)
+    print("\nFuncions\n--------------------")
+    for i in range(len(menu)):
+        print(f"{i+1}. {menu[i]}")
+    print(f"{mida+1}. SORTIR")
+
+# Funció de multiplicació
+def crear_matriu_2d(n1,n2):
+    '''Imprimeix el resultat de multiplicar dos números passats 
+    com a paràmetres, els inputs es fan a fora de la funció.'''
+    m = [[random.randint(1,100) for i in range(n2)] for i in range(n1)]
+    print(m)
+
+
+def fer_llista_unica(m):
+    m_unica = list(set([num for row in m for num in row]))
+    return m_unica
+
+
+def cercar_els_que_acaben_en(m,e):
+    None
+
+# Funció principal
+def main():
+    '''Anem demanant que vol fer l'usuari i
+    depenent la resposta inicialitzem una funció
+    o un altre.'''
+    
+    n1 = int(input("Introdueix el primer nombre: "))
+    n2 = int(input("Introdueix el segon nombre: "))
+    m = crear_matriu_2d(n1,n2)
+    
+
+    print(fer_llista_unica(m))
+
+
+    
+    print(cercar_els_que_acaben_en(m))
+    
+
+    
+    print(cercar_els_que_acaben_en(m))    
+        
+    
+if __name__ == "__main__":
+    main()
