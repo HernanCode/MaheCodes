b63deaf6fcb2689fea2036d322892a38d2e92bac
Dane <dan.maldonado.2132@lacetania.cat>
2023-12-11 20:10:02 +0100

    cambios

--- SamuelDan_MP03UF2_Matrius.py
+++ SamuelDan_MP03UF2_Matrius.py
@@ -4,11 +4,10 @@ import utils
 # Ex1
 def crear_matriu_2d(n1, n2):
     m = [[random.randint(1, 100) for i in range(n2)] for i in range(n1)]
-    print(m)
     return m
 
 # Ex2
-def print_matrix(m):
+def imprimir_matriu(m):
     for row in m:
         print(*row)
 
@@ -37,13 +36,33 @@ def crear_quadrat(s,c):
     quadrat = [[c for j in range(s)] for i in range(s)]
     return quadrat
 
+
+
+# Ex8
+def inicialitzar_matriu(q,c):
+    matriu = [[c for j in i] for i in q]
+    for row in matriu:
+        print(*row)
+
+def diagonal(q,c):
+    for i in range(len(q)):
+        q[i][i] = c
+    return q
+
+# Ex9
+def posar_en_diagonal(q, c):
+    diagonal(q,c)
+    diagonal(q[::-1],c)
+    return q
+
+
 # Funció principal
 def main():
     n1 = utils.secure_int("Introdueix el primer nombre: ")
     n2 = utils.secure_int("Introdueix el segon nombre: ")
     m = crear_matriu_2d(n1, n2)
     
-    print_matrix(m)
+    imprimir_matriu(m)
     
     n = utils.secure_int("Introdueix un nombre (1-10): ", 1, 10)
     print(llistar_multiples(m,n))
@@ -56,7 +75,14 @@ def main():
     print(cercar_els_que_acaben_en(m,e))
     
     s = utils.secure_int("Introdueix un nombre (10-20): ", 10, 20)
-    print(crear_quadrat(s,"*"))
+    crear_quadrat(s,"*")
+
+    q = crear_matriu_2d(5, 5)
+    inicialitzar_matriu(q,"-")
+
+    q = crear_quadrat(19,"-")
+    diagonal = posar_en_diagonal(q,"*")
+    imprimir_matriu(diagonal)
     
 if __name__ == "__main__":
     main()
